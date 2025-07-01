086595cb4e3d83182156520b5053518d17e34731
Sushrut Chandra <sushrutchandra29@gmail.com>
2025-06-30 20:37:19 -0400

    Create Blackjack.java

---
+++ Blackjack.java
@@ -0,0 +1,78 @@
+package blackjack;
+import java.util.*;
+
+public class Blackjack implements BlackjackEngine {
+	
+	
+	public Blackjack(Random randomGenerator, int numberOfDecks) {
+	    throw new UnsupportedOperationException();
+	}
+	
+	public int getNumberOfDecks() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void createAndShuffleGameDeck() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public Card[] getGameDeck() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void deal() {	
+		throw new UnsupportedOperationException();
+	}
+		
+	public Card[] getDealerCards() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int[] getDealerCardsTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int getDealerCardsEvaluation() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public Card[] getPlayerCards() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public int[] getPlayerCardsTotal() {
+		throw new UnsupportedOperationException();
+	}
+		
+	public int getPlayerCardsEvaluation() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void playerHit() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void playerStand() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getGameStatus() {
+		throw new UnsupportedOperationException();
+	}
+		
+	public void setBetAmount(int amount) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getBetAmount() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void setAccountAmount(int amount) {	
+		throw new UnsupportedOperationException();
+	}
+	
+	public int getAccountAmount() {
+		throw new UnsupportedOperationException();
+	}
+}
